6c69624bebcaf99d780b844c15579c3b21c9af8b
yescine <bouchoucha.yassine@gmail.com>
2023-03-15 19:53:38 +0100

    basics creat-react app hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Welcome page
+      <Routes>
+        <Route path="/" Component={() => <div>homepage</div>} />
+        <Route path="/users" Component={() => <div>users</div>} />
+        <Route path="*" Component={() => <div>not Found</div>} />
+      </Routes>
     </div>
   );
 }
